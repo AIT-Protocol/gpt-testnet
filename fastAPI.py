bf6e13439d0880ba11325829d8ec165b06c5cf11
Nguyễn Đoàn Xuân Bình <86184794+ndxbinh1922001@users.noreply.github.com>
2024-06-24 14:37:08 +0700

    update code

--- fastAPI.py
+++ fastAPI.py
@@ -1,80 +1,62 @@
-from fastapi import FastAPI,UploadFile,File,Form
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.exceptions import HTTPException
-import matplotlib as plt
 import PIL.Image
-from gemini import Gemini
 import io
 import uvicorn
-gemini = Gemini()
-gemini.setup()
-app = FastAPI()
 from openAI import OpenAI
 import argparse
+import os
+from dotenv import load_dotenv, find_dotenv
+
+# Argument Parsing
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_name", help="The name of the model",
+                    default="gpt-3.5-turbo-0125", required=False)
+parser.add_argument("--max_tokens", help="Used to specify the maximum number of tokens the model can generate which is the length of the response. The default value is 2048", default=2048, required=False)
+parser.add_argument(
+    "--temperature", help="Used to specify the temperature of the model which controls the creativeness of the model. The default value is 0.9", default=0.9, required=False)
+parser.add_argument("--top_k", help="It's like having a lot of ideas but only picking the few best ones to talk about. This makes the text make more sense. Reducing the number ensures that the model's choices are among the most probable, leading to more coherent text. The default value is 50", default=50)
+parser.add_argument("--top_p", help="This is like choosing ideas that together make a good story, instead of just picking the absolute best ones. It helps the text be both interesting and sensible. The default value is 0.9", default=0.9, required=False)
+
+args = parser.parse_args()
+print(args.model_name)
+
+# Load environment variables
+load_dotenv(find_dotenv())
+
+# Initialize OpenAI
+openai = OpenAI(model_name=args.model_name, max_tokens=args.max_tokens,
+                temperature=args.temperature, top_p=args.top_p)
+
+# FastAPI setup
+app = FastAPI()
 
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
+
+@app.post("/submit/")
+async def submit(input_text: str = Form(...)):
+    return openai.get_messages(input_text)
+
+
+@app.post("/multiple")
+async def multiple(input_text: str = Form(...), img: UploadFile = File(...)):
+    img.file.seek(0, 2)
+    file_size = img.file.tell()
+    await img.seek(0)
+    print("file size: ", file_size)
+    if file_size > 20 * 1024 * 1024:
+        raise HTTPException(status_code=400, detail="File too large")
+
+    content_type = img.content_type
+    if content_type not in ["image/jpeg", "image/png", "image/gif"]:
+        raise HTTPException(status_code=400, detail="Invalid file type")
+    tempfile = await img.read()
+    img = PIL.Image.open(io.BytesIO(tempfile))
+    return openai.generate_from_TxtandImg(input_text, img)
 
 if __name__ == "__main__":
-    parser =argparse.ArgumentParser()
-    parser.add_argument("--model_name",help="The name of the model",default="gpt-3.5-turbo-0125",required=False)
-    parser.add_argument("--max_tokens",help=" used to specify the maximum number of tokens the model can generate which is the length of the response. The default value is 2048",default=2048,required=False)
-    parser.add_argument("--temperature",help=" used to specify the temperature of the model which controls the creativeness of the model. The default value is 0.9",default=0.9,required=False)
-    parser.add_argument("--top_k",help=" It's like having a lot of ideas but only picking the few best ones to talk about. This makes the text make more sense. Reducing the number ensures that the model's choices are among the most probable, leading to more coherent text. The default value is 50",default=50)
-    parser.add_argument("--top_p",help="This is like choosing ideas that together make a good story, instead of just picking the absolute best ones. It helps the text be both interesting and sensible. The default value is 0.9",default=0.9,required=False)
-
-    args = parser.parse_args()
-    print(args.model_name)
-    openai= OpenAI(model_name=args.model_name,max_tokens=args.max_tokens,temperature=args.temperature,top_p=args.top_p)
-
-    @app.get("/")  # giống flask, khai báo phương thức get và url
-    async def root():  # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
-        return {"message": "Hello World"}
-
-
-    @app.post("/multiple")
-    async def multiple(promt: str, image: UploadFile = File(...)):
-        image.file.seek(0, 2)
-        file_size = image.file.tell()
-        await image.seek(0)
-
-        if file_size > 20 * 1024 * 1024:
-            raise HTTPException(status_code=400, detail="File too large")
-
-        content_type = image.content_type
-        if content_type not in ["image/jpeg", "image/png", "image/gif"]:
-            raise HTTPException(status_code=400, detail="Invalid file type")
-        tempfile = await image.read()
-        img = PIL.Image.open(io.BytesIO(tempfile))
-        # do something with the valid file
-        return gemini.generate_from_ImgandTxt(promt, img)
-
-
-    @app.post("/image")
-    async def multiple(image: UploadFile = File(...)):
-
-        image.file.seek(0, 2)
-        file_size = image.file.tell()
-        await image.seek(0)
-
-        if file_size > 20 * 1024 * 1024:
-            raise HTTPException(status_code=400, detail="File too large")
-
-        content_type = image.content_type
-        if content_type not in ["image/jpeg", "image/png", "image/gif"]:
-            raise HTTPException(status_code=400, detail="Invalid file type")
-        # do something with the valid file
-        tempfile = await image.read()
-        img = PIL.Image.open(io.BytesIO(tempfile))
-        return gemini.generate_from_img(img)
-
-
-    @app.post("/prompt")
-    async def multiple(prompt: str = Form(...)):
-
-        return gemini.generate_from_txt(prompt)
-
-
-    @app.post("/submit/")
-    async def submit(prom: str = Form(...)):
-        return openai.get_messages(prom)
     uvicorn.run(app, host="127.0.0.1", port=5049)
--- openAI.py
+++ openAI.py
@@ -1,20 +1,18 @@
-
-from langchain.chat_models import ChatOpenAI
-from langchain.schema.messages import HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_community.callbacks import get_openai_callback
 from langchain_core.output_parsers import StrOutputParser
-import  os
+import os
 from dotenv import load_dotenv, find_dotenv
+# import logging
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
 
 class OpenAI:
-    def __init__(self,model_name,max_tokens,temperature,top_p):
+    def __init__(self, model_name, max_tokens, temperature, top_p):
         _ = load_dotenv(find_dotenv())
         api_key = os.environ.get("OPENAI_API_KEY")
         print(api_key)
-
         self.model = ChatOpenAI(
             api_key=api_key,
             model_name=model_name,
@@ -22,37 +20,25 @@ class OpenAI:
             temperature=temperature,
             top_p=top_p
         )
-        promtFile = open('prompt.txt', 'r')
-        self.system_prompt =promtFile.read()
-        promtFile.close()
-    def get_messages(self,input):
-        prompt = ChatPromptTemplate.from_messages([("system", self.system_prompt), ("user", "{input}")])
+        with open('prompt.txt', 'r') as promtFile:
+            self.system_prompt = promtFile.read()
+
+    def get_messages(self, input_text):
+        prompt = ChatPromptTemplate.from_messages(
+            [("system", self.system_prompt), ("user", "{input}")])
         chain = prompt | self.model | StrOutputParser()
-        role = "user"
-        response = chain.invoke({"role": role, "input": input})
+        response = chain.invoke({"role": "user", "input": input_text})
+        print("chain: {}".format(chain))
+        print("response: {}".format(response))
         return response
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def generate_from_TxtandImg(self, input_text, img):
+        # try:
+            # model = genai.GenerativeModel(model_name='gemini-pro-vision')
+        response = self.model.generate_content(
+            [input_text, img], stream=True)
+        print("response: {}".format(response))
+        response.resolve()
+        return response.text
+        # except Exception as exception:
+        #     raise Exception("An error occurred during the process:", exception)
